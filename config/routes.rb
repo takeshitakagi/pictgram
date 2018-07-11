defd2efdced8cf8ff737ec0052af470b21c673aa
takeshi <takagitakeshi2@gmail.com>
2018-07-12 01:53:16 +0900

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+
+  get 'topics/new'
+  get 'topics/index'
+
+  get 'sessions/new'
+
+  root 'pages#index'
+  get 'pages/help'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+end
---
+++ vendor/bundle/ruby/2.4.0/gems/ffi-1.9.25/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/takeshi/pictgram/vendor/bundle/ruby/2.4.0/gems/ffi-1.9.25/ext/ffi_c/libffi/src/x86/ffitarget.h
